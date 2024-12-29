f29603fd55a163adc4742807375a9b01ed6f85e4
jigsaw <jigsawsmma@gmail.com>
2024-12-29 17:57:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<div align="center">
+<img alt="preview" src="https://github.com/SkidGod4444/portfolio/blob/main/public/prev.png" width="90%">
+</div>
+
+# Portfolio [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FSkidGod4444%2Fportfolio)
+
+Built with next.js, [loops](https://loops.so/), [shadcn/ui](https://ui.shadcn.com/), [acternity ui](https://ui.aceternity.com/), [magic ui](https://magicui.design/) and [Supabase](https://supabase.com/dashboard/projects), deployed on Vercel.
+
+# Features
+
+- Setup only takes a few minutes by editing the [single config file](./src/data/config/site.config.tsx)
+- Built using Next.js 14, React, Typescript, Shadcn/UI, TailwindCSS, Framer Motion, Magic UI, Acternity UI, Loops and Supabase
+- Includes a blog, newsletter, about, projects and guestbook pages
+- Responsive for different devices
+- Optimized for Next.js and Vercel
+
+# Getting Started Locally
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/SkidGod4444/portfolio
+   ```
+
+2. Move to the cloned directory
+
+   ```bash
+   cd portfolio
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   bun install
+   ```
+
+4. Start the local Server:
+
+   ```bash
+   bun dev
+   ```
+
+5. Open the [Config file](./src/data/config/site.config.tsx) and make changes
+
+6. Set up your [Loops](https://loops.so/) account and add your API key to the `env.local` file as mentioned in `.env.example`
+   
+7. Set up your [Supabase](https://supabase.com/dashboard/projects) account and add your API key to the `env.local` file as mentioned in `.env.example`
+
+# License
+
+Licensed under the [MIT license](https://github.com/SkidGod4444/portfolio/blob/main/LICENSE.md).
+
+# Credits
+
+- Special thanks to [Dillion Verma](https://github.com/dillionverma/portfolio) for his base template!
---
+++ src/app/layout.tsx
@@ -0,0 +1,122 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { DATA } from "@/data/config/site.config";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { TopNavbar } from "@/components/top-navbar";
+import BottomNavbar from "@/components/bottom-navbar";
+import { ScheduleCallFloat } from "@/components/cal";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "@/components/ui/sonner";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(DATA.url),
+  title: {
+    default: DATA.name,
+    template: `%s - ${DATA.name}`,
+  },
+  description: DATA.description,
+  keywords: [
+    "Saidev Dhal",
+    "SkidGod",
+    "Saidev Dhal Portfolio",
+    "Saidev Dhal Projects",
+    "Saidev Dhal Blogs",
+    "Saidev Dhal Resume",
+    "Saidev Dhal Contact",
+    "Saidev Dhal Instagram",
+    "Saidev Dhal Youtube",
+    "Saidev Dhal Email",
+    "Saidev Dhal LinkedIn",
+    "Saidev Dhal GitHub",
+    "Saidev Dhal Twitter",
+    "saidevdhal",
+    "skidgod",
+    "skidgod4444",
+  ],
+  authors: [
+    {
+      name: `${DATA.name}`,
+      url: DATA.url,
+    },
+  ],
+  creator: `${DATA.name}`,
+  openGraph: {
+    title: `${DATA.name}`,
+    description: DATA.description,
+    url: DATA.url,
+    images: [
+      {
+        url: DATA.prevImage,
+      },
+    ],
+    siteName: `${DATA.name}`,
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: `${DATA.name}`,
+    card: "summary_large_image",
+    site: DATA.url,
+    creator: `${DATA.name}`,
+    description: DATA.description,
+    images: [
+      {
+        url: DATA.prevImage,
+        width: 1200,
+        height: 630,
+        alt: `${DATA.name}`,
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <TooltipProvider delayDuration={0}>
+            <div className="hidden md:block">
+              <TopNavbar />
+            </div>
+            {children}
+            <div className="block md:hidden">
+              <BottomNavbar />
+            </div>
+            <ScheduleCallFloat />
+            <Analytics />
+            <Toaster />
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
